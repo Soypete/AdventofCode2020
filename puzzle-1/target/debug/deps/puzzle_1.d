b4947b67805a3529b42b922f44f6cb5f2dc06b32
Soypete <34897832+Soypete@users.noreply.github.com>
2020-12-02 19:39:22 -0700

    solution-1

---
+++ puzzle-1/target/debug/deps/puzzle_1.d
@@ -0,0 +1,5 @@
+/Users/miriahpeterson/Code/advent2020/puzzle-1/target/debug/deps/puzzle_1: src/main.rs
+
+/Users/miriahpeterson/Code/advent2020/puzzle-1/target/debug/deps/puzzle_1.d: src/main.rs
+
+src/main.rs:
---
+++ puzzle-1/target/debug/puzzle-1.d
@@ -0,0 +1,1 @@
+/Users/miriahpeterson/Code/advent2020/puzzle-1/target/debug/puzzle-1: /Users/miriahpeterson/Code/advent2020/puzzle-1/src/main.rs
